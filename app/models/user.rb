599d89ac4684768de52ee8e8e01a2deae23f522f
ChernomorYegor <chernomor.yegor@gmail.com>
2020-01-13 22:00:08 +0300

    added model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = ^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$
+	validates :email, format: { with: VALID_EMAIL_REGEX }
 end
