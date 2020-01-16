a7d87de2330078027a2c6f2be6d64bb966938fc7
ChernomorYegor <chernomor.yegor@gmail.com>
2020-01-16 21:47:22 +0300

    added view show and gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   # get 'users/new'
   get 'signup', to: 'users#new'
+  get 'show', to: 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
