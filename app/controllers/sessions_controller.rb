c291c62af4e8c9c11c0c6119b89afff9c3f9ffae
ChernomorYegor <chernomor.yegor@gmail.com>
2020-01-27 22:09:52 +0300

    added sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,13 @@
 class SessionsController < ApplicationController
+	include SessionsHelper
   def new
   end
 
   def create
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
+			log_in(user)
+			remember(user)
 			redirect_to user
 		else
 			flash[:danger] = 'Invalid email/password combination'
@@ -12,4 +15,9 @@ class SessionsController < ApplicationController
 		end
 	end
 
+	def destroy
+		log_out
+		flash[:danger] = 'Logged out'
+		redirect_to root_url
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   get 'show', to: 'users#show'
 
   resources :users
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
