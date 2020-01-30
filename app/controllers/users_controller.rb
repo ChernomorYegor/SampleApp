0c14f24a078ed56b2ebbf4f01192ceb881d35cb1
ChernomorYegor <chernomor.yegor@gmail.com>
2020-01-30 22:20:39 +0300

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:edit, :update]
+	before_action :correct_user, only: [:edit, :update]
+
+	def index
+		@users = User.paginate(page: params[:page], per_page: 20)
+	end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -10,6 +17,7 @@ class UsersController < ApplicationController
   def create
 		@user = User.new(user_params)
 		if @user.save
+			log_in @user
 			flash[:success] = "User created. Welcome to the Sample App!"
 			redirect_to @user
 		else
@@ -17,7 +25,46 @@ class UsersController < ApplicationController
 		end
 	end
 
+	def edit
+		# @user = User.find(params[:id])
+	end
+
+	def update
+		# @user = User.find(params[:id])
+		if @user.update(user_params)
+		flash[:success] = "User updated!"
+		redirect_to edit_user_url(@user)
+		else
+		render 'edit'
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		if @user.destroy
+			flash[:success] = "User killed!"
+		else
+			flash[:danger] = "Destroy failed"
+		end
+		redirect_to users_url
+	end
+
+	private
+
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
+
+	def logged_in_user
+		unless logged_in?
+			flash[:danger] = "Please log in."
+			redirect_to new_session_url
+		end
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless @user == current_user
+	end
+
 end
